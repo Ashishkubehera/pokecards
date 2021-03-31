58eaf381b45fca0b1eb78d86cc2546d2c1db8164
Ashish Ku Behera <74497927+Ashishkubehera@users.noreply.github.com>
2021-03-31 21:20:52 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component
+{
+  constructor(){
+    super();
+
+    this.state={
+      monster: []
+    };
+  }
+
+  componentDidMount()
+  {
+    fetch("http://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => this.setState({monster: users}));
+  }
+
+  render()
+  {
+    return (
+      <div className="App">
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello <code>live</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        {
+          this.state.monster.map(
+            monster =>
+            <h1 key={monster.id}>
+               { monster.name } 
+            </h1>)
+        }
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
